2fd200f342222a931a4443e60a2f941d67b4a03e
danielalonsomendez <danielalonso308@gmail.com>
2026-01-12 12:24:12 +0100

    Initial commit

---
+++ Elorserv/src/main/java/com/reto2/elorserv/ElorservApplication.java
@@ -0,0 +1,13 @@
+package com.reto2.elorserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElorservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElorservApplication.class, args);
+	}
+
+}
